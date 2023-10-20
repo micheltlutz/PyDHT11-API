493294f4af7f764480ad9ab6339d64be9f6f9a2f
Michel Anderson Lutz Teixeira <michel_lutz@icloud.com>
2023-10-20 12:24:08 -0300

    Add FastAPI

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import Adafruit_DHT
-import time
 import logging
 
-# Configurações do logger
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+from decouple import config
+from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse
+from fastapi.responses import RedirectResponse
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+app = FastAPI()
 
 # Definir o tipo de sensor
 DHT_SENSOR = Adafruit_DHT.DHT11
@@ -11,17 +17,50 @@ DHT_SENSOR = Adafruit_DHT.DHT11
 # Definir o pino ao qual o sensor está conectado
 DHT_PIN = 4  # Considerando que o sensor esteja conectado ao GPIO4
 
-def read_dht11_data():
+
+@app.get("/")
+def redirect_to_docs():
+    response = RedirectResponse(url='/docs')
+    return response
+
+
+@app.get("/health-check")
+def health_check():
+    return JSONResponse(
+        content={"status": "ok"},
+        status_code=status.HTTP_200_OK
+    )
+
+
+@app.get("/version")
+def health_check():
+    return JSONResponse(
+        content={"version": config("VERSION")},
+        status_code=status.HTTP_200_OK
+    )
+
+
+@app.get("/dht11", response_model=dict)
+def get_dht11_data():
+    """
+    Busca os dados de umidade e temperatura do sensor DHT11
+    """
     try:
         humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        
         if humidity is not None and temperature is not None:
-            logging.info(f"Temperatura={temperature:.2f}C  Umidade={humidity:.2f}%")
+            return {
+                "temperature": f"{temperature:.2f}C",
+                "humidity": f"{humidity:.2f}%"
+            }
         else:
-            logging.error("Falha ao receber a leitura do sensor DHT11. Tentando novamente...")
+            logging.error("Falha ao receber a leitura do sensor DHT11.")
+            raise HTTPException(status_code=500, detail="Erro na leitura do sensor DHT11.")
     except Exception as e:
         logging.error(f"Erro ao ler o sensor DHT11: {e}")
+        raise HTTPException(status_code=500, detail="Erro interno do servidor.")
+
 
 if __name__ == "__main__":
-    while True:
-        read_dht11_data()
-        time.sleep(2)  # Aguarde 2 segundos antes da próxima leitura
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
